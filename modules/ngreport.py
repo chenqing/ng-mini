2deecb70dfe97d934174642f7b9d51a0b0f88574
chenqing <root@chenqing.org>
2013-10-10 17:00:15 +0800

    report data

---
+++ modules/ngreport.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys
+import os
+import subprocess
+
+import nginit
+from nglog import log
+
+def report_mysql(host,user,passwd,database,data):
+    """
+    report collect data to mysql
+    """
+
+def report_mongo():
+    """
+    report collect data to mongodb
+    """
+
+
+def report_redis():
+    """
+    report collect data to redis
+    """
+
+
+def report_memcache():
+    """
+    report collect data to memcache
+    """
+
+def report_http_post():
+    """
+    report collect data use http post
+    """
+
+
